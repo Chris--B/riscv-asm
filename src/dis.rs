b6902786d5e13fb5a0df36b09ed4f3aec7b6c944
Chris Butler <chrisbutler296@gmail.com>
2020-07-25 00:35:28 -0400

    ✨ Add a Goblin-Based Hex Dump

--- src/dis.rs
+++ src/dis.rs
@@ -1,5 +1,8 @@
+use std::fs;
 use std::path::Path;
 
+use goblin::elf::Elf;
+use goblin::Object;
 use gumdrop::Options;
 
 #[derive(Debug, Options)]
@@ -50,7 +53,72 @@ impl MyOptions {
     }
 }
 
-fn main() {
+fn extract_code<'a>(elf: &'a Elf, buffer: &'a [u8]) -> Vec<u32> {
+    use goblin::elf::program_header::*;
+
+    let load_headers: Vec<&ProgramHeader> = elf
+        .program_headers
+        .iter()
+        .filter(|hdr| hdr.p_type == PT_LOAD)
+        .collect();
+
+    assert_eq!(load_headers.len(), 1);
+    let header = load_headers[0];
+    assert_eq!(header.p_memsz % 4, 0);
+
+    let start = header.p_offset as usize;
+    let end = start + header.p_memsz as usize;
+    let bytes = &buffer[start..end];
+
+    bytes
+        .chunks_exact(4)
+        .map(|w| u32::from_le_bytes([w[0], w[1], w[2], w[3]]))
+        .collect()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = MyOptions::new();
     dbg!(&opts);
+
+    let buffer: Vec<u8> = fs::read(&opts.input)?;
+    let elf: Elf = match Object::parse(&buffer)? {
+        Object::Elf(elf) => elf,
+        Object::PE(_pe) => {
+            eprintln!("{}: Expected ELF, found PE", opts.input);
+            return Ok(());
+        }
+        Object::Mach(_mach) => {
+            eprintln!("{}: Expected ELF, found MACH", opts.input);
+            return Ok(());
+        }
+        Object::Archive(_archive) => {
+            eprintln!("{}: Expected ELF, found ARCHIVE", opts.input);
+            return Ok(());
+        }
+        Object::Unknown(magic) => {
+            eprintln!(
+                "{}: Expected ELF, found unknown format (magic: {:#x}",
+                opts.input, magic
+            );
+            return Ok(());
+        }
+    };
+
+    // Dump the entire Object struct to disk to debug it
+    let debug_path = format!("{}.debug", opts.output.unwrap());
+    fs::write(debug_path, &format!("{:#?}", elf))?;
+
+    let code: Vec<u32> = extract_code(&elf, &buffer);
+
+    // Hex Dump
+    const WORDS_PER_LINE: usize = 4;
+    for (idx, four_words) in code.as_slice().chunks(WORDS_PER_LINE).enumerate() {
+        print!("  0x{:>03x}: ", WORDS_PER_LINE * idx);
+        for word in four_words {
+            print!("0x{:08x} ", word);
+        }
+        println!();
+    }
+
+    Ok(())
 }
