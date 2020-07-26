c387501ba79180718fe18b15976e14952f6eede5
Chris Butler <chrisbutler296@gmail.com>
2020-07-26 01:23:53 -0400

    ✨ Basic OpCode Decode Works!
    After disassembling the right section (not segment), and adjusting
    for some SYSTEM opcodes, things appear to work!

--- src/dis.rs
+++ src/dis.rs
@@ -58,26 +58,24 @@ impl MyOptions {
 }
 
 fn extract_code<'a>(elf: &'a Elf, buffer: &'a [u8]) -> Vec<u32> {
-    use goblin::elf::program_header::*;
+    for section in &elf.section_headers {
+        let name = &elf.shdr_strtab[section.sh_name];
 
-    let load_headers: Vec<&ProgramHeader> = elf
-        .program_headers
-        .iter()
-        .filter(|hdr| hdr.p_type == PT_LOAD)
-        .collect();
+        if name == ".text" {
+            let start = section.sh_offset as usize;
+            let end = start + section.sh_size as usize;
+            let bytes = &buffer[start..end];
 
-    assert_eq!(load_headers.len(), 1);
-    let header = load_headers[0];
-    assert_eq!(header.p_filesz % 4, 0);
+            println!("Found {} bytes of code in \".text\"", bytes.len());
 
-    let start = header.p_offset as usize;
-    let end = start + header.p_filesz as usize;
-    let bytes = &buffer[start..end];
+            return bytes
+                .chunks_exact(4)
+                .map(|w| u32::from_le_bytes([w[0], w[1], w[2], w[3]]))
+                .collect();
+        }
+    }
 
-    bytes
-        .chunks_exact(4)
-        .map(|w| u32::from_le_bytes([w[0], w[1], w[2], w[3]]))
-        .collect()
+    panic!("No '.text' section in elf")
 }
 
 struct Word(u32);
@@ -162,6 +160,7 @@ struct UnknownInstr {
     rs1: u32,
     rs2: u32,
     funct7: u32,
+    funct12: u32,
 }
 
 fn fmt_hex_bin(x: u32) -> String {
@@ -178,6 +177,7 @@ impl fmt::Debug for UnknownInstr {
             .field("rs1", &fmt_hex_bin(self.rs1))
             .field("rs2", &fmt_hex_bin(self.rs2))
             .field("funct7", &fmt_hex_bin(self.funct7))
+            .field("funct12", &fmt_hex_bin(self.funct12))
             .finish()
     }
 }
@@ -190,95 +190,92 @@ fn decode_opcode(w: Word) -> String {
     let rs1 = w.bits(19, 15);
     let rs2 = w.bits(24, 20);
     let funct7 = w.bits(31, 25);
+    let funct12 = w.bits(31, 20);
 
     assert!(funct3 < (1 << 3));
     assert!(funct7 < (1 << 7));
 
-    match (opcode, funct3, funct7) {
+    match (opcode, funct3) {
         // Load Instructions
-        (0x03, 0x0, _) => ("lb").into(),
-        (0x03, 0x1, _) => ("lh").into(),
-        (0x03, 0x2, _) => ("lw").into(),
-        (0x03, 0x3, _) => ("ld").into(),
-        (0x03, 0x4, _) => ("lbu").into(),
-        (0x03, 0x5, _) => ("lhu").into(),
-        (0x03, 0x6, _) => ("lwu").into(),
+        (0x03, 0x0) => ("lb").into(),
+        (0x03, 0x1) => ("lh").into(),
+        (0x03, 0x2) => ("lw").into(),
+        (0x03, 0x3) => ("ld").into(),
+        (0x03, 0x4) => ("lbu").into(),
+        (0x03, 0x5) => ("lhu").into(),
+        (0x03, 0x6) => ("lwu").into(),
 
         // Fences
-        (0x0f, 0x0, _) => ("fence").into(),
-        (0x0f, 0x1, _) => ("fence.i").into(),
+        (0x0f, 0x0) => ("fence").into(),
+        (0x0f, 0x1) => ("fence.i").into(),
 
-        // Arithmetic
-        (0x13, 0x0, _) => ("addi").into(),
-        (0x13, 0x1, 0x00) => ("slli").into(),
-        (0x13, 0x2, _) => ("slti").into(),
-        (0x13, 0x3, _) => ("sltiu").into(),
-        (0x13, 0x4, _) => ("xori").into(),
-        (0x13, 0x5, 0x00) => ("srli").into(),
-        (0x13, 0x5, 0x20) => ("srai").into(),
-        (0x13, 0x6, _) => ("ori").into(),
-        (0x13, 0x7, _) => ("andi").into(),
+        (0x13, 0x0) => ("addi").into(),
+        (0x13, 0x1) if funct7 == 0x00 => ("slli").into(),
+        (0x13, 0x2) => ("slti").into(),
+        (0x13, 0x3) => ("sltiu").into(),
+        (0x13, 0x4) => ("xori").into(),
+        (0x13, 0x5) if funct7 == 0x00 => ("srli").into(),
+        (0x13, 0x5) if funct7 == 0x20 => ("srai").into(),
+        (0x13, 0x6) => ("ori").into(),
+        (0x13, 0x7) => ("andi").into(),
 
-        // ???
-        (0x17, _, _) => ("auipc").into(),
+        (0x17, _) => ("auipc").into(),
 
-        // More shifting?
-        (0x1b, 0x0, _) => ("addiw").into(),
-        (0x1b, 0x1, 0x00) => ("slliw").into(),
-        (0x1b, 0x5, 0x00) => ("srliw").into(),
-        (0x1b, 0x5, 0x20) => ("sraiw").into(),
+        (0x1b, 0x0) => ("addiw").into(),
+        (0x1b, 0x1) if funct7 == 0x00 => ("slliw").into(),
+        (0x1b, 0x5) if funct7 == 0x00 => ("srliw").into(),
+        (0x1b, 0x5) if funct7 == 0x20 => ("sraiw").into(),
 
         // Store Instructions
-        (0x23, 0x0, _) => ("sb").into(),
-        (0x23, 0x1, _) => ("sh").into(),
-        (0x23, 0x2, _) => ("sw").into(),
-        (0x23, 0x3, _) => ("sd").into(),
+        (0x23, 0x0) => ("sb").into(),
+        (0x23, 0x1) => ("sh").into(),
+        (0x23, 0x2) => ("sw").into(),
+        (0x23, 0x3) => ("sd").into(),
 
-        // Some R-types
-        (0x33, 0x0, 0x00) => ("add").into(),
-        (0x33, 0x0, 0x20) => ("sub").into(),
-        (0x33, 0x1, _) => ("sll").into(),
-        (0x33, 0x2, _) => ("slt").into(),
-        (0x33, 0x3, _) => ("sltu").into(),
-        (0x33, 0x4, _) => ("xor").into(),
-        (0x33, 0x5, 0x00) => ("srl").into(),
-        (0x33, 0x5, 0x20) => ("sra").into(),
-        (0x33, 0x6, _) => ("or").into(),
-        (0x33, 0x7, _) => ("and").into(),
+        (0x33, 0x0) if funct7 == 0x00 => ("add").into(),
+        (0x33, 0x0) if funct7 == 0x20 => ("sub").into(),
+        (0x33, 0x1) => ("sll").into(),
+        (0x33, 0x2) => ("slt").into(),
+        (0x33, 0x3) => ("sltu").into(),
+        (0x33, 0x4) => ("xor").into(),
+        (0x33, 0x5) if funct7 == 0x00 => ("srl").into(),
+        (0x33, 0x5) if funct7 == 0x20 => ("sra").into(),
+        (0x33, 0x6) => ("or").into(),
+        (0x33, 0x7) => ("and").into(),
 
-        // ???
-        (0x37, _, _) => ("lui").into(),
+        (0x37, _) => ("lui").into(),
 
-        // ???
-        (0x3b, 0x0, 0x00) => ("addw").into(),
-        (0x3b, 0x0, 0x20) => ("subw").into(),
-        (0x3b, 0x1, 0x00) => ("sllw").into(),
-        (0x3b, 0x5, 0x00) => ("srlw").into(),
-        (0x3b, 0x5, 0x20) => ("sraw").into(),
+        (0x3b, 0x0) if funct7 == 0x00 => ("addw").into(),
+        (0x3b, 0x0) if funct7 == 0x20 => ("subw").into(),
+        (0x3b, 0x1) if funct7 == 0x00 => ("sllw").into(),
+        (0x3b, 0x5) if funct7 == 0x00 => ("srlw").into(),
+        (0x3b, 0x5) if funct7 == 0x20 => ("sraw").into(),
 
-        // ???
-        (0x63, 0x0, _) => ("beq").into(),
-        (0x63, 0x1, _) => ("bne").into(),
-        (0x63, 0x4, _) => ("blt").into(),
-        (0x63, 0x5, _) => ("bge").into(),
-        (0x63, 0x6, _) => ("bltu").into(),
-        (0x63, 0x7, _) => ("bgeu").into(),
+        (0x63, 0x0) => ("beq").into(),
+        (0x63, 0x1) => ("bne").into(),
+        (0x63, 0x4) => ("blt").into(),
+        (0x63, 0x5) => ("bge").into(),
+        (0x63, 0x6) => ("bltu").into(),
+        (0x63, 0x7) => ("bgeu").into(),
 
-        (0x67, 0x0, _) => ("jalr").into(),
+        (0x67, 0x0) => ("jalr").into(),
 
-        (0x6f, _, _) => ("jal").into(),
+        (0x6f, _) => ("jal").into(),
 
-        (0x73, 0x0, 0x0) => ("ecall").into(),
-        (0x73, 0x0, 0x1) => ("ebreak").into(),
-        (0x73, 0x1, _) => ("csrrw").into(),
-        (0x73, 0x2, _) => ("csrrs").into(),
-        (0x73, 0x3, _) => ("csrrc").into(),
-        (0x73, 0x5, _) => ("csrrwi").into(),
-        (0x73, 0x6, _) => ("csrrsi").into(),
-        (0x73, 0x7, _) => ("csrrci").into(),
+        (0x73, 0x0) if funct7 == 0x0 => ("ecall").into(),
+        (0x73, 0x0) if funct7 == 0x1 => ("ebreak").into(),
+        (0x73, 0x0) if funct12 == 0x302 => ("wfi").into(),
+        (0x73, 0x0) if funct12 == 0x105 => ("mret").into(),
+
+        (0x73, 0x1) => ("csrrw").into(),
+        (0x73, 0x2) => ("csrrs").into(),
+        (0x73, 0x3) => ("csrrc").into(),
+        (0x73, 0x5) => ("csrrwi").into(),
+        (0x73, 0x6) => ("csrrsi").into(),
+        (0x73, 0x7) => ("csrrci").into(),
 
         _ => {
-            if false {
+            if true {
                 let info = UnknownInstr {
                     word: w.0,
                     opcode,
@@ -287,8 +284,9 @@ fn decode_opcode(w: Word) -> String {
                     rs1,
                     rs2,
                     funct7,
+                    funct12,
                 };
-                format!("{:?}", info)
+                format!("{:#?}", info)
             } else {
                 format!("??? opcode=0x{:x} (0x{:08x})", opcode, w.0)
             }
@@ -348,7 +346,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     const INSTR_PER_LINE: usize = 1;
 
     for (idx, four_words) in code.as_slice().chunks(INSTR_PER_LINE).enumerate() {
-        print!("  0x{:>03x}: ", INSTR_PER_LINE * idx);
+        let addr = std::mem::size_of::<u32>() * INSTR_PER_LINE * idx;
+        print!("  0x{:>03x}: ", addr);
         for word in four_words {
             let instr = decode_opcode(Word(*word));
             print!("{:<25}", instr);
