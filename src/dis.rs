d7d5ab6399b853b6ed10a5c12f703e1d70623b19
Chris Butler <chrisbutler296@gmail.com>
2020-07-26 22:41:58 -0400

    🎨🚧 Parse Instructions into an Enum
    This will make it easier to do things programmatically to the code,
    rather than just having another String to parse.
    
    Still needs to handle immediates though

--- src/dis.rs
+++ src/dis.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::inconsistent_digit_grouping)]
 #![deny(unreachable_patterns)]
 
-use std::fmt;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::fs;
 use std::path::Path;
 
@@ -151,35 +152,390 @@ fn check_word_bits_1() {
     }
 }
 
-#[derive(Copy, Clone)]
-struct UnknownInstr {
-    word: u32,
-    opcode: u32,
-    rd: u32,
-    funct3: u32,
-    rs1: u32,
-    rs2: u32,
-    funct7: u32,
-    funct12: u32,
+/// Register mnemonics for the standard ABI
+///
+/// See: https://github.com/riscv/riscv-elf-psabi-doc/blob/master/riscv-elf.md#integer-register-convention-
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Reg {
+    Zero = 0,
+    Ra = 1,
+    Sp = 2,
+    Gp = 3,
+    Tp = 4,
+    T0 = 5,
+    T1 = 6,
+    T2 = 7,
+    S0 = 8,
+    S1 = 9,
+    A0 = 10,
+    A1 = 11,
+    A2 = 12,
+    A3 = 13,
+    A4 = 14,
+    A5 = 15,
+    A6 = 16,
+    A7 = 17,
+    S2 = 18,
+    S3 = 19,
+    S4 = 20,
+    S5 = 21,
+    S6 = 22,
+    S7 = 23,
+    S8 = 24,
+    S9 = 25,
+    S10 = 26,
+    S11 = 27,
+    T3 = 28,
+    T4 = 29,
+    T5 = 30,
+    T6 = 31,
 }
 
-fn fmt_hex_bin(x: u32) -> String {
-    format!("(0x{:x}, 0b_{:b})", x, x)
-}
-
-impl fmt::Debug for UnknownInstr {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.debug_struct("UnknownInstr")
-            .field("word", &fmt_hex_bin(self.word))
-            .field("opcode", &fmt_hex_bin(self.opcode))
-            .field("rd", &fmt_hex_bin(self.rd))
-            .field("funct3", &fmt_hex_bin(self.funct3))
-            .field("rs1", &fmt_hex_bin(self.rs1))
-            .field("rs2", &fmt_hex_bin(self.rs2))
-            .field("funct7", &fmt_hex_bin(self.funct7))
-            .field("funct12", &fmt_hex_bin(self.funct12))
-            .finish()
+impl Default for Reg {
+    fn default() -> Reg {
+        Reg::Zero
     }
+}
+
+/// An error when a register is referenced out of bounds
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct RegIndexError {
+    idx: u32,
+}
+
+impl TryFrom<u32> for Reg {
+    type Error = RegIndexError;
+    fn try_from(idx: u32) -> Result<Reg, Self::Error> {
+        let o_reg = match idx {
+            0 => Some(Reg::Zero),
+            1 => Some(Reg::Ra),
+            2 => Some(Reg::Sp),
+            3 => Some(Reg::Gp),
+            4 => Some(Reg::Tp),
+            5 => Some(Reg::T0),
+            6 => Some(Reg::T1),
+            7 => Some(Reg::T2),
+            8 => Some(Reg::S0),
+            9 => Some(Reg::S1),
+            10 => Some(Reg::A0),
+            11 => Some(Reg::A1),
+            12 => Some(Reg::A2),
+            13 => Some(Reg::A3),
+            14 => Some(Reg::A4),
+            15 => Some(Reg::A5),
+            16 => Some(Reg::A6),
+            17 => Some(Reg::A7),
+            18 => Some(Reg::S2),
+            19 => Some(Reg::S3),
+            20 => Some(Reg::S4),
+            21 => Some(Reg::S5),
+            22 => Some(Reg::S6),
+            23 => Some(Reg::S7),
+            24 => Some(Reg::S8),
+            25 => Some(Reg::S9),
+            26 => Some(Reg::S10),
+            27 => Some(Reg::S11),
+            28 => Some(Reg::T3),
+            29 => Some(Reg::T4),
+            30 => Some(Reg::T5),
+            31 => Some(Reg::T6),
+            _ => None,
+        };
+
+        if let Some(reg) = o_reg {
+            Ok(reg)
+        } else {
+            Err(RegIndexError { idx })
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug)]
+enum Instr {
+    Lb {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Lh {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Lw {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Ld {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Lbu {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Lhu {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Lwu {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Fence {
+        rd: Reg,
+        rs1: Reg,
+
+        /// Successor Write/Read/Device Output/DeviceInput
+        ///
+        /// 4-bit value
+        successor: u8,
+
+        /// Predecessor Write/Read/Device Output/DeviceInput
+        ///
+        /// 4-bit value
+        predecessor: u8,
+
+        /// Fence Mode
+        ///
+        /// 4-bit value
+        fm: u8,
+    },
+    FenceI {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Addi {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Slli {
+        rd: Reg,
+        rs1: Reg,
+        imm5: u8,
+    },
+    Slti {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Sltiu {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Xori {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Srli {
+        rd: Reg,
+        rs1: Reg,
+        imm5: u8,
+    },
+    Srai {
+        rd: Reg,
+        rs1: Reg,
+        imm5: u8,
+    },
+    Ori {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Andi {
+        rd: Reg,
+        rs1: Reg,
+        imm12: u32,
+    },
+    Auipc {
+        rd: Reg,
+        imm20: u32,
+    },
+    Sb {
+        /// Base address
+        rs1: Reg,
+        /// Source register
+        rs2: Reg,
+        /// Offset
+        imm12: u32,
+    },
+    Sh {
+        /// Base address
+        rs1: Reg,
+        /// Source register
+        rs2: Reg,
+        /// Offset
+        imm12: u32,
+    },
+    Sw {
+        /// Base address
+        rs1: Reg,
+        /// Source register
+        rs2: Reg,
+        /// Offset
+        imm12: u32,
+    },
+    Sd {
+        /// Base address
+        rs1: Reg,
+        /// Source register
+        rs2: Reg,
+        /// Offset
+        imm12: u32,
+    },
+    Add {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Sub {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Sll {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Slt {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Sltu {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Xor {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Srl {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Sra {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Or {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    And {
+        rd: Reg,
+        rs1: Reg,
+        rs2: Reg,
+    },
+    Lui {
+        rd: Reg,
+        imm20: u32,
+    },
+    Beq {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+    Bne {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+    Blt {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+    Bge {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+    Bltu {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+    Bgeu {
+        rs1: Reg,
+        rs2: Reg,
+        imm12: u32,
+    },
+
+    /// Jumps to a target address and saves the return address
+    ///
+    /// The target address is obtained by adding the sign-extended `imm12`
+    /// to the register `rs1` then setting the LSB to 0.
+    /// The instruction following the jump (pc + 4) is written to register `rd`
+    /// The standard software calling convention uses `x1` as the return address
+    /// register and `x5` as an alternative link register.
+    Jalr {
+        rd: Reg,
+        rs1: Reg,
+        /// Encoded as a multiple of 2-bytes
+        imm12: u32,
+    },
+
+    /// Jumps to a relative address
+    ///
+    /// `imm20` is sign-extended and added to the address of the jump
+    /// instruction to form the jump target address.
+    /// The standard software calling convention uses `x1` as the return address
+    /// register and `x5` as an alternative link register.
+    Jal {
+        rd: Reg,
+        /// Encoded as a multiple of 2-bytes
+        imm20: u32,
+    },
+
+    /// Make a service request to the execution environment
+    ///
+    /// The EEI will define how parameters for the service request are passed,
+    /// but usually these will be in define locations in the integer register
+    /// file.
+    Ecall {
+        rd: Reg,
+        rs1: Reg,
+    },
+
+    /// Return control to a debugging environment
+    Ebreak {
+        rd: Reg,
+        rs1: Reg,
+    },
+
+    // TODO: System Instructions
+    Wfi {},
+    Mret {},
+    Csrrw {},
+    Csrrs {},
+    Csrrc {},
+    Csrrwi {},
+    Csrrsi {},
+    Csrrci {},
+
+    Hint {
+        /// TODO: Encode hint instructions
+        /// Most of them use rd == x0 as a reserved space
+        hint: (),
+    },
 }
 
 #[allow(unused_variables)]
@@ -220,124 +576,110 @@ fn decode_opcode(w: Word) -> String {
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         |    opcode   |    rd   |               imm[31;12]              |
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
     */
 
     // We extract each field value here, then reference them in the
-    /// larger match block below.
+    // larger match block below.
     let opcode = w.bits(6, 0);
-    let rd = w.bits(11, 7);
+    let rd: Reg = w.bits(11, 7).try_into().unwrap_or_default();
     let funct3 = w.bits(14, 12);
-    let rs1 = w.bits(19, 15);
-    let rs2 = w.bits(24, 20);
+    let rs1: Reg = w.bits(19, 15).try_into().unwrap_or_default();
+    let rs2: Reg = w.bits(24, 20).try_into().unwrap_or_default();
     let funct7 = w.bits(31, 25);
     let funct12 = w.bits(31, 20);
 
-    assert!(funct3 < (1 << 3));
-    assert!(funct7 < (1 << 7));
+    // TODO: Not sure if these are always loaded the same way
+    let imm5 = 0;
+    let imm12 = 0;
+    let imm20 = 0;
 
-    match (opcode, funct3) {
+    let o_instr: Option<Instr> = match (opcode, funct3) {
         // Load Instructions
-        (0x03, 0x0) => ("lb").into(),
-        (0x03, 0x1) => ("lh").into(),
-        (0x03, 0x2) => ("lw").into(),
-        (0x03, 0x3) => ("ld").into(),
-        (0x03, 0x4) => ("lbu").into(),
-        (0x03, 0x5) => ("lhu").into(),
-        (0x03, 0x6) => ("lwu").into(),
+        (0x03, 0x0) => Some(Instr::Lb { rd, rs1, imm12 }),
+        (0x03, 0x1) => Some(Instr::Lh { rd, rs1, imm12 }),
+        (0x03, 0x2) => Some(Instr::Lw { rd, rs1, imm12 }),
+        (0x03, 0x3) => Some(Instr::Ld { rd, rs1, imm12 }),
+        (0x03, 0x4) => Some(Instr::Lbu { rd, rs1, imm12 }),
+        (0x03, 0x5) => Some(Instr::Lhu { rd, rs1, imm12 }),
+        (0x03, 0x6) => Some(Instr::Lwu { rd, rs1, imm12 }),
 
         // Fences
-        (0x0f, 0x0) => ("fence").into(),
-        (0x0f, 0x1) => ("fence.i").into(),
+        (0x0f, 0x0) => Some(Instr::Fence {
+            rd,
+            rs1,
+            successor: 0,
+            predecessor: 0,
+            fm: 0,
+        }),
+        (0x0f, 0x1) => Some(Instr::FenceI { rd, rs1, imm12 }),
 
-        (0x13, 0x0) => ("addi").into(),
-        (0x13, 0x1) if funct7 == 0x00 => ("slli").into(),
-        (0x13, 0x2) => ("slti").into(),
-        (0x13, 0x3) => ("sltiu").into(),
-        (0x13, 0x4) => ("xori").into(),
-        (0x13, 0x5) if funct7 == 0x00 => ("srli").into(),
-        (0x13, 0x5) if funct7 == 0x20 => ("srai").into(),
-        (0x13, 0x6) => ("ori").into(),
-        (0x13, 0x7) => ("andi").into(),
+        (0x13, 0x0) => Some(Instr::Addi { rd, rs1, imm12 }),
+        (0x13, 0x1) if funct7 == 0x00 => Some(Instr::Slli { rd, rs1, imm5 }),
+        (0x13, 0x2) => Some(Instr::Slti { rd, rs1, imm12 }),
+        (0x13, 0x3) => Some(Instr::Sltiu { rd, rs1, imm12 }),
+        (0x13, 0x4) => Some(Instr::Xori { rd, rs1, imm12 }),
+        (0x13, 0x5) if funct7 == 0x00 => Some(Instr::Srli { rd, rs1, imm5 }),
+        (0x13, 0x5) if funct7 == 0x20 => Some(Instr::Srai { rd, rs1, imm5 }),
+        (0x13, 0x6) => Some(Instr::Ori { rd, rs1, imm12 }),
+        (0x13, 0x7) => Some(Instr::Andi { rd, rs1, imm12 }),
 
-        (0x17, _) => ("auipc").into(),
+        (0x17, _) => Some(Instr::Auipc { rd, imm20 }),
 
-        (0x1b, 0x0) => ("addiw").into(),
-        (0x1b, 0x1) if funct7 == 0x00 => ("slliw").into(),
-        (0x1b, 0x5) if funct7 == 0x00 => ("srliw").into(),
-        (0x1b, 0x5) if funct7 == 0x20 => ("sraiw").into(),
+        // // Store Instructions
+        (0x23, 0x0) => Some(Instr::Sb { rs1, rs2, imm12 }),
+        (0x23, 0x1) => Some(Instr::Sh { rs1, rs2, imm12 }),
+        (0x23, 0x2) => Some(Instr::Sw { rs1, rs2, imm12 }),
+        (0x23, 0x3) => Some(Instr::Sd { rs1, rs2, imm12 }),
 
-        // Store Instructions
-        (0x23, 0x0) => ("sb").into(),
-        (0x23, 0x1) => ("sh").into(),
-        (0x23, 0x2) => ("sw").into(),
-        (0x23, 0x3) => ("sd").into(),
+        (0x33, 0x0) if funct7 == 0x00 => Some(Instr::Add { rd, rs1, rs2 }),
+        (0x33, 0x0) if funct7 == 0x20 => Some(Instr::Sub { rd, rs1, rs2 }),
+        (0x33, 0x1) => Some(Instr::Sll { rd, rs1, rs2 }),
+        (0x33, 0x2) => Some(Instr::Slt { rd, rs1, rs2 }),
+        (0x33, 0x3) => Some(Instr::Sltu { rd, rs1, rs2 }),
+        (0x33, 0x4) => Some(Instr::Xor { rd, rs1, rs2 }),
+        (0x33, 0x5) if funct7 == 0x00 => Some(Instr::Srl { rd, rs1, rs2 }),
+        (0x33, 0x5) if funct7 == 0x20 => Some(Instr::Sra { rd, rs1, rs2 }),
+        (0x33, 0x6) => Some(Instr::Or { rd, rs1, rs2 }),
+        (0x33, 0x7) => Some(Instr::And { rd, rs1, rs2 }),
 
-        (0x33, 0x0) if funct7 == 0x00 => ("add").into(),
-        (0x33, 0x0) if funct7 == 0x20 => ("sub").into(),
-        (0x33, 0x1) => ("sll").into(),
-        (0x33, 0x2) => ("slt").into(),
-        (0x33, 0x3) => ("sltu").into(),
-        (0x33, 0x4) => ("xor").into(),
-        (0x33, 0x5) if funct7 == 0x00 => ("srl").into(),
-        (0x33, 0x5) if funct7 == 0x20 => ("sra").into(),
-        (0x33, 0x6) => ("or").into(),
-        (0x33, 0x7) => ("and").into(),
+        (0x37, _) => Some(Instr::Lui { rd, imm20 }),
 
-        (0x37, _) => ("lui").into(),
+        (0x63, 0x0) => Some(Instr::Beq { rs1, rs2, imm12 }),
+        (0x63, 0x1) => Some(Instr::Bne { rs1, rs2, imm12 }),
+        (0x63, 0x4) => Some(Instr::Blt { rs1, rs2, imm12 }),
+        (0x63, 0x5) => Some(Instr::Bge { rs1, rs2, imm12 }),
+        (0x63, 0x6) => Some(Instr::Bltu { rs1, rs2, imm12 }),
+        (0x63, 0x7) => Some(Instr::Bgeu { rs1, rs2, imm12 }),
 
-        (0x3b, 0x0) if funct7 == 0x00 => ("addw").into(),
-        (0x3b, 0x0) if funct7 == 0x20 => ("subw").into(),
-        (0x3b, 0x1) if funct7 == 0x00 => ("sllw").into(),
-        (0x3b, 0x5) if funct7 == 0x00 => ("srlw").into(),
-        (0x3b, 0x5) if funct7 == 0x20 => ("sraw").into(),
+        (0x67, 0x0) => Some(Instr::Jalr { rd, rs1, imm12 }),
 
-        (0x63, 0x0) => ("beq").into(),
-        (0x63, 0x1) => ("bne").into(),
-        (0x63, 0x4) => ("blt").into(),
-        (0x63, 0x5) => ("bge").into(),
-        (0x63, 0x6) => ("bltu").into(),
-        (0x63, 0x7) => ("bgeu").into(),
+        (0x6f, _) => Some(Instr::Jal { rd, imm20 }),
 
-        (0x67, 0x0) => ("jalr").into(),
+        (0x73, 0x0) if funct7 == 0x0 => Some(Instr::Ecall { rd, rs1 }),
+        (0x73, 0x0) if funct7 == 0x1 => Some(Instr::Ebreak { rd, rs1 }),
+        (0x73, 0x0) if funct12 == 0x302 => Some(Instr::Wfi {}),
+        (0x73, 0x0) if funct12 == 0x105 => Some(Instr::Mret {}),
 
-        (0x6f, _) => ("jal").into(),
+        (0x73, 0x1) => Some(Instr::Csrrw {}),
+        (0x73, 0x2) => Some(Instr::Csrrs {}),
+        (0x73, 0x3) => Some(Instr::Csrrc {}),
+        (0x73, 0x5) => Some(Instr::Csrrwi {}),
+        (0x73, 0x6) => Some(Instr::Csrrsi {}),
+        (0x73, 0x7) => Some(Instr::Csrrci {}),
+        _ => None,
+    };
 
-        (0x73, 0x0) if funct7 == 0x0 => ("ecall").into(),
-        (0x73, 0x0) if funct7 == 0x1 => ("ebreak").into(),
-        (0x73, 0x0) if funct12 == 0x302 => ("wfi").into(),
-        (0x73, 0x0) if funct12 == 0x105 => ("mret").into(),
-
-        (0x73, 0x1) => ("csrrw").into(),
-        (0x73, 0x2) => ("csrrs").into(),
-        (0x73, 0x3) => ("csrrc").into(),
-        (0x73, 0x5) => ("csrrwi").into(),
-        (0x73, 0x6) => ("csrrsi").into(),
-        (0x73, 0x7) => ("csrrci").into(),
-
-        _ => {
-            if true {
-                let info = UnknownInstr {
-                    word: w.0,
-                    opcode,
-                    rd,
-                    funct3,
-                    rs1,
-                    rs2,
-                    funct7,
-                    funct12,
-                };
-                format!("{:#?}", info)
-            } else {
-                format!("??? opcode=0x{:x} (0x{:08x})", opcode, w.0)
-            }
-        }
+    if let Some(instr) = o_instr {
+        format!("{:?}", o_instr)
+    } else {
+        format!("opcode= 0x{:x}, 0b_{:b}", opcode, opcode)
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = MyOptions::new();
     dbg!(&opts);
+    dbg!(std::mem::size_of::<Instr>());
 
     let buffer: Vec<u8> = fs::read(&opts.input)?;
     let elf: Elf = match Object::parse(&buffer)? {
